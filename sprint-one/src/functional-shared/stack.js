22cce2a6f40ff4d43287ed6d053a170cd841ba50
comesm <comesm@gmail.com>
2016-12-14 15:07:02 -0800

    Make first 3 tests of functional shared stack shared behavior pass

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,20 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {
+    length: 0,
+    storage: {}
+  };
+  return _.extend(obj, stackMethods);
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function() { return this.length; },
+  push: function( value ) {
+    this.length++;
+    this.storage[value] = value;
+  },
+  pop: function() {}
+};
 
 
