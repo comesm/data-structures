0286678b5592a2fdc8ef0df6b4ced95875cdc9c5
comesm <comesm@gmail.com>
2016-12-14 15:46:16 -0800

    Made last test of first half of prototypal stack pattern pass

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -22,4 +22,7 @@ stackMethods.pop = function() {
   if (this.length !== 0) {
     this.length--;
   }
+  var temp = this.storage[Object.keys(this.storage)[0]];
+  delete this.storage[Object.keys(this.storage)[0]];
+  return temp;
 };
