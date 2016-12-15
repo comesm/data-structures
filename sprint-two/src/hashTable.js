1832eb16c75fcc6c6cda0362bc6e30235bc74621
comesm <comesm@gmail.com>
2016-12-15 11:28:25 -0800

    Implement insert, retrieve and remove methods.  Make tests 1-5 pass

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,14 +5,21 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.each(function(value, storageIndex, storageArray) {
+    if (storageIndex === index) {
+      storageArray.splice(storageIndex, 1);
+    }
+  });
 };
 
 
