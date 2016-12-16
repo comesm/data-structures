1ad362b7f67c194dcfc554ecb6e07f651de189a3
comesm <comesm@gmail.com>
2016-12-16 10:53:09 -0800

    Refator hashTable to use arrays for storage instead of objects

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,31 +4,46 @@ var HashTable = function() {
 };
 //O(1)
 HashTable.prototype.insert = function(k, v) {
-  var obj = {};
-  obj[k] = v;
+  var tuple = [];
+  tuple.push([k, v]);
   var index = getIndexBelowMaxForKey(k, this._limit);
   if ( this._storage.get(index) === undefined ) {
-    this._storage.set(index, obj);  
+    this._storage.set(index, tuple);
   } else {
     var temp = this._storage.get(index);
-    temp[k] = v;
-    this._storage.set(index, temp);
+    if (this.retrieve(k) === undefined) {
+      temp.push([k, v]);
+      this._storage.set(index, temp);
+    } else {
+      for ( var i = 0; i < temp.length; i++) {
+        if ( temp[i][0] === k ) {
+          temp[i][1] = v;
+        }
+      }
+      this._storage.set(index, temp);
+    }
   }
 };
 //O(1)
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var result = this._storage.get(index);
-  return result[k];
+  for ( var i = 0; i < result.length; i++) {
+    var tuple = result[i];
+    console.log(tuple);
+    if (tuple[0] === k) {
+      return tuple[1];
+    }
+  }
 };
 //O(n)
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.each(function(value, storageIndex, storageArray) {
+  this._storage.each(function(tupleArray, storageIndex, storageArray) {
     if (storageIndex === index) {
-      for ( var key in value ) {
-        if (key === k) {
-          delete value[k];
+      for ( var i = 0; i < tupleArray.length; i++) {
+        if ( tupleArray[i][0] === k ) {
+          tupleArray.splice(tupleArray.indexOf(tupleArray[i]), 1);
         }
       }
     }
