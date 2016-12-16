1f0bf86b6d31ff925adca633fedb15ea83a47bd9
comesm <comesm@gmail.com>
2016-12-15 16:30:42 -0800

    Create methods for BST and make first test for BST pass

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,18 @@
 var BinarySearchTree = function(value) {
+  var newBsTree = {};
+  newBsTree.value = value;
+  newBsTree.right = undefined;
+  newBsTree.left = undefined;
+  return _.extend(newBsTree, bsTreeMethods);
 };
+
+var bsTreeMethods = {};
+
+bsTreeMethods.insert = function() {};
+
+bsTreeMethods.contains = function() {};
+
+bsTreeMethods.depthFirstLog = function() {};
 
 
 /*
